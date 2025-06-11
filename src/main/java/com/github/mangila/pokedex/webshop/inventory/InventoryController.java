8a41b8e9ee11849e1dbe1e5e7091808e79fadc68
mangila <olsson.erik1993@gmail.com>
2025-06-11 15:25:37 +0200

    enterprise architect

---
+++ src/main/java/com/github/mangila/pokedex/webshop/inventory/InventoryController.java
@@ -0,0 +1,7 @@
+package com.github.mangila.pokedex.webshop.inventory;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class InventoryController {
+}
---
+++ src/main/java/com/github/mangila/pokedex/webshop/inventory/InventoryService.java
@@ -0,0 +1,7 @@
+package com.github.mangila.pokedex.webshop.inventory;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryService {
+}
