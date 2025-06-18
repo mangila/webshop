481fab0bdbd0aea8ff139c9a5d7ec294e59d0dec
mangila <olsson.erik1993@gmail.com>
2025-06-18 21:43:19 +0200

    command

--- src/main/java/com/github/mangila/webshop/product/model/ProductNotFoundException.java
+++ src/main/java/com/github/mangila/webshop/product/model/ProductNotFoundException.java
@@ -2,6 +2,6 @@ package com.github.mangila.webshop.product.model;
 
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(String id) {
-        super(String.format("Product with id %s was not found", id));
+        super(String.format("Product with id '%s' was not found", id));
     }
 }
