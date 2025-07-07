7a75464e154e97560a42332d1531b34a57182ee1
Erik Olsson <olsson.erik1993@gmail.com>
2025-07-07 21:52:20 +0200

    service validation

--- backend/src/main/java/com/github/mangila/webshop/shared/uuid/application/UuidGeneratorService.java
+++ backend/src/main/java/com/github/mangila/webshop/shared/uuid/application/UuidGeneratorService.java
@@ -2,7 +2,8 @@ package com.github.mangila.webshop.shared.uuid.application;
 
 import com.github.mangila.webshop.shared.uuid.domain.UuidRecord;
 import com.github.mangila.webshop.shared.uuid.domain.UuidRecordRepository;
-import jakarta.validation.constraints.NotNull;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
@@ -17,12 +18,14 @@ public class UuidGeneratorService {
         this.repository = repository;
     }
 
+    @CacheEvict(value = "domain-id", allEntries = true)
     public UUID generate(GenerateNewUuidIntent intent) {
         Assert.notNull(intent, "GenerateNewUuidIntent must not be null");
         var record = UuidRecord.create(intent.value());
         return repository.save(record).getId();
     }
 
+    @Cacheable(value = "domain-id")
     public boolean hasGenerated(UUID id) {
         return repository.existsById(id);
     }
