9b32d0abb48010116bb3533fe8335d721014d7f8
Erik Olsson <olsson.erik1993@gmail.com>
2025-07-07 21:37:53 +0200

    service validation

--- backend/src/main/java/com/github/mangila/webshop/shared/infrastructure/json/JsonMapper.java
+++ backend/src/main/java/com/github/mangila/webshop/shared/infrastructure/json/JsonMapper.java
@@ -3,15 +3,12 @@ package com.github.mangila.webshop.shared.infrastructure.json;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mangila.webshop.shared.domain.exception.ApplicationException;
+import graphql.Assert;
 import io.vavr.control.Try;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JsonMapper {
-
-    private static final Logger log = LoggerFactory.getLogger(JsonMapper.class);
 
     private final ObjectMapper objectMapper;
 
@@ -20,16 +17,9 @@ public class JsonMapper {
     }
 
     public JsonNode toJsonNode(Object object) {
-        return readTree(object);
-    }
-
-    private JsonNode readTree(Object object) {
         return Try.of(() -> {
-                    JsonNode jsonNode = objectMapper.valueToTree(object);
-                    if (jsonNode.isNull()) {
-                        return objectMapper.createObjectNode();
-                    }
-                    return jsonNode;
+                    Assert.assertNotNull(object, "Object must not be null");
+                    return (JsonNode) objectMapper.valueToTree(object);
                 })
                 .getOrElseThrow(cause -> new ApplicationException(String.format("Error parsing object: %s", object), cause));
     }
