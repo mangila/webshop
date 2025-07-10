a8a08a86c741558e006b8da2637382e020331bab
Erik Olsson <olsson.erik1993@gmail.com>
2025-07-10 22:46:44 +0200

    cash money

--- backend/src/main/java/com/github/mangila/webshop/shared/infrastructure/spring/validation/CurrencyValidator.java
+++ backend/src/main/java/com/github/mangila/webshop/shared/infrastructure/spring/validation/CurrencyValidator.java
@@ -2,6 +2,7 @@ package com.github.mangila.webshop.shared.infrastructure.spring.validation;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.apache.commons.lang3.ArrayUtils;
 import org.joda.money.CurrencyUnit;
 import org.springframework.util.StringUtils;
 
@@ -14,7 +15,7 @@ public class CurrencyValidator implements ConstraintValidator<Currency, String> 
 
     @Override
     public void initialize(Currency constraintAnnotation) {
-        if (constraintAnnotation.currencies().length > 0) {
+        if (ArrayUtils.isEmpty(constraintAnnotation.currencies())) {
             this.validCurrencies = Arrays.stream(constraintAnnotation.currencies())
                     .map(CurrencyUnit::of)
                     .toList();
