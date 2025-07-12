e0b5d7e846e9a8fc8641536ccec1cf855485783c
Erik Olsson <olsson.erik1993@gmail.com>
2025-07-12 16:30:21 +0200

    rabbitmq consumer

--- backend/src/main/java/com/github/mangila/webshop/inventory/infrastructure/config/InventoryRabbitMqConfig.java
+++ backend/src/main/java/com/github/mangila/webshop/inventory/infrastructure/config/InventoryRabbitMqConfig.java
@@ -6,12 +6,15 @@ import com.github.mangila.webshop.shared.application.registry.RegistryService;
 import com.github.mangila.webshop.shared.infrastructure.config.RabbitMqConfig;
 import com.rabbitmq.stream.Environment;
 import com.rabbitmq.stream.Message;
+import io.micrometer.common.KeyValues;
 import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.rabbit.stream.config.StreamRabbitListenerContainerFactory;
 import org.springframework.rabbit.stream.listener.StreamListenerContainer;
+import org.springframework.rabbit.stream.micrometer.RabbitStreamListenerObservationConvention;
+import org.springframework.rabbit.stream.micrometer.RabbitStreamMessageReceiverContext;
 
 import java.util.function.Predicate;
 
@@ -26,13 +29,29 @@ public class InventoryRabbitMqConfig {
     }
 
     @Bean
-    public RabbitListenerContainerFactory<StreamListenerContainer> inventoryNewProductConsumer(Environment env) {
+    public RabbitListenerContainerFactory<StreamListenerContainer> inventoryNewProductConsumer(Environment environment) {
         var streamKey = RabbitMqConfig.PRODUCT_STREAM_KEY;
         var domain = Domain.from("PRODUCT", registryService);
         var event = Event.from("PRODUCT_CREATE_NEW", registryService);
-        StreamRabbitListenerContainerFactory factory = new StreamRabbitListenerContainerFactory(env);
+        StreamRabbitListenerContainerFactory factory = new StreamRabbitListenerContainerFactory(environment);
         factory.setNativeListener(Boolean.TRUE);
         factory.setObservationEnabled(Boolean.TRUE);
+        factory.setStreamListenerObservationConvention(new RabbitStreamListenerObservationConvention() {
+            @Override
+            public String getName() {
+                return "rabbit-mq-stream-listener";
+            }
+
+            @Override
+            public String getContextualName(RabbitStreamMessageReceiverContext context) {
+                return context.getSource().concat(": ").concat(context.getListenerId());
+            }
+
+            @Override
+            public KeyValues getLowCardinalityKeyValues(RabbitStreamMessageReceiverContext context) {
+                return KeyValues.of("domain", domain.value()).and("event", event.value());
+            }
+        });
         factory.setConsumerCustomizer((_, builder) -> builder.name("inventoryNewProductConsumer")
                 .stream(streamKey)
                 .filter()
--- backend/src/main/java/com/github/mangila/webshop/inventory/infrastructure/message/InventoryRabbitMqConsumer.java
+++ backend/src/main/java/com/github/mangila/webshop/inventory/infrastructure/message/InventoryRabbitMqConsumer.java
@@ -4,8 +4,6 @@ import com.github.mangila.webshop.shared.infrastructure.config.RabbitMqConfig;
 import com.github.mangila.webshop.shared.infrastructure.json.JsonMapper;
 import com.github.mangila.webshop.shared.outbox.infrastructure.message.OutboxMessage;
 import com.rabbitmq.stream.Message;
-import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.tracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -17,19 +15,15 @@ public class InventoryRabbitMqConsumer {
     private static final Logger log = LoggerFactory.getLogger(InventoryRabbitMqConsumer.class);
 
     private final JsonMapper jsonMapper;
-    private final Tracer tracer;
-    private final ObservationRegistry registry;
 
-    public InventoryRabbitMqConsumer(JsonMapper jsonMapper, Tracer tracer, ObservationRegistry registry) {
+    public InventoryRabbitMqConsumer(JsonMapper jsonMapper) {
         this.jsonMapper = jsonMapper;
-        this.tracer = tracer;
-        this.registry = registry;
     }
 
     @RabbitListener(
+            id = "inventoryNewProductConsumer",
             queues = RabbitMqConfig.PRODUCT_STREAM_KEY,
-            containerFactory = "inventoryNewProductConsumer",
-            id = "inventoryNewProductConsumer")
+            containerFactory = "inventoryNewProductConsumer")
     void listen(Message message) {
         OutboxMessage event = jsonMapper.toObject(message.getBodyAsBinary(), OutboxMessage.class);
         log.info("Consumed Message: {}", event);
