0d98f4afd8a22c3b543f81db039b5677a88f4c0a
Erik Olsson <olsson.erik1993@gmail.com>
2025-07-18 14:54:18 +0200

    message proccess

---
+++ service/message/src/main/java/com/github/mangila/webshop/message/OutboxRelay.java
@@ -0,0 +1,18 @@
+package com.github.mangila.webshop.message;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class OutboxRelay {
+
+    @Transactional
+    @Scheduled(fixedRate = 5, timeUnit = TimeUnit.SECONDS)
+    void poll() {
+       // var outboxMessages = repository.query().findAllByPublished(false, 10);
+    }
+
+}
