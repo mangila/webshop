d6ca8229b370d870a2cb372929370867ae88af86
Erik Olsson <olsson.erik1993@gmail.com>
2025-07-19 17:04:01 +0200

    wip

--- service/outbox/src/main/java/com/github/mangila/webshop/outbox/infrastructure/message/MessageRelay.java
+++ service/outbox/src/main/java/com/github/mangila/webshop/outbox/infrastructure/message/MessageRelay.java
@@ -2,6 +2,9 @@ package com.github.mangila.webshop.outbox.infrastructure.message;
 
 import com.github.mangila.webshop.outbox.domain.message.OutboxMessage;
 import com.github.mangila.webshop.outbox.infrastructure.message.spring.SpringEventPublisher;
+import io.vavr.control.Try;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -13,6 +16,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 @Service
 public class MessageRelay {
 
+    private static final Logger log = LoggerFactory.getLogger(MessageRelay.class);
     private final BlockingQueue<OutboxMessage> messageQueue;
     private final SpringEventPublisher publisher;
     private volatile boolean running = true;
@@ -29,11 +33,17 @@ public class MessageRelay {
     @Async
     @EventListener(ApplicationReadyEvent.class)
     void run() {
-        while (running) {
-            OutboxMessage message = messageQueue.poll();
-            if (message != null) {
-                publisher.publish(message);
+        Try.of(() -> {
+            while (running) {
+                OutboxMessage message = messageQueue.poll();
+                if (message != null) {
+                    publisher.publish(message);
+                }
             }
-        }
+            return null;
+        }).onFailure(throwable -> {
+            log.error("Error while relaying messages", throwable);
+            running = false;
+        });
     }
 }
