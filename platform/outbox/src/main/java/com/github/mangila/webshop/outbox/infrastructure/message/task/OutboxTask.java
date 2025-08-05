fc4776361cc42c1a6fc6cca73f017ae6b4ed5269
Erik Olsson <olsson.erik1993@gmail.com>
2025-08-05 14:16:45 +0200

    clean up task

--- platform/outbox/src/main/java/com/github/mangila/webshop/outbox/infrastructure/message/task/OutboxTask.java
+++ platform/outbox/src/main/java/com/github/mangila/webshop/outbox/infrastructure/message/task/OutboxTask.java
@@ -1,9 +1,6 @@
 package com.github.mangila.webshop.outbox.infrastructure.message.task;
 
-public sealed interface OutboxTask permits
-        FillQueueOutboxTask,
-        ProcessDlqOutboxTask,
-        ProcessQueueOutboxTask {
+public sealed interface OutboxTask permits CleanUpPublishedOutboxTask, FillQueueOutboxTask, ProcessDlqOutboxTask, ProcessQueueOutboxTask {
     void execute();
 
     OutboxTaskKey key();
