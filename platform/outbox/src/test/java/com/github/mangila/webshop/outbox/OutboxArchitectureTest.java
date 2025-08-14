1307654e2463db344b4f372efca53512b674acf8
Erik Olsson <olsson.erik1993@gmail.com>
2025-08-14 13:10:26 +0200

    archunit

--- platform/outbox/src/test/java/com/github/mangila/webshop/outbox/OutboxArchitectureTest.java
+++ platform/outbox/src/test/java/com/github/mangila/webshop/outbox/OutboxArchitectureTest.java
@@ -4,13 +4,15 @@ import com.github.mangila.webshop.shared.SimpleJob;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 public class OutboxArchitectureTest {
 
     @Test
-    void jobClassesShouldNotDependOnOtherPackages() {
+    @DisplayName("Should have job classes ending with Job or SimpleJob")
+    void jobClassesShouldEndWithJobOrSimpleJob() {
         JavaClasses jobs = new ClassFileImporter()
                 .importPackages("com.github.mangila.webshop.outbox.infrastructure.scheduler.job");
         ArchRuleDefinition.classes()
